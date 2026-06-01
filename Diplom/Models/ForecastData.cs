aee60f4b3e87e4cbfb948dc312938c9577395ecd
goddanshi <oceanfluddy@gmail.com>
2026-06-01 23:49:24 +0300

    Remove ML.NET attributes from ForecastData model

--- Diplom/Models/ForecastData.cs
+++ Diplom/Models/ForecastData.cs
@@ -1,25 +1,15 @@
-﻿using Microsoft.ML.Data;
-
 namespace Diplom.Models
 {
     public class ForecastData
     {
-        [LoadColumn(0)]
         public DateTime Date { get; set; }
-
-        [LoadColumn(1)]
         public float TotalVolume { get; set; }
     }
 
     public class ForecastOutput
     {
-        [ColumnName("ForecastedVolume")]
-        public float[] Forecast { get; set; }
-
-        [ColumnName("LowerBound")]
-        public float[] LowerBound { get; set; }
-
-        [ColumnName("UpperBound")]
-        public float[] UpperBound { get; set; }
+        public float[] Forecast { get; set; } = Array.Empty<float>();
+        public float[] LowerBound { get; set; } = Array.Empty<float>();
+        public float[] UpperBound { get; set; } = Array.Empty<float>();
     }
 }
